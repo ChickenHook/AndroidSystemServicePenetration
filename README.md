a201e11face66fc783bae9ba519b7d1fff66cf79
SarotecK <funker2013@gmail.com>
2022-10-20 19:55:06 +0200

    add mroe tests

--- README.md
+++ README.md
@@ -8,3 +8,13 @@ To customize this you have to do the following:
 
 1. Rename build.xml project name to your project name
 2. Change myAndroidHackingLibrary to your project name
+
+# Video descriptin
+
+[![](http://img.youtube.com/vi/-_be1LBz7eA/0.jpg)](http://www.youtube.com/watch?v=-_be1LBz7eA "Description video")
+
+# Bug / Exploit documentation
+
+https://androidreverse.wordpress.com/2022/10/19/parcel-java-oom-exploit/?preview_id=309&preview_nonce=a3138eeb6f&preview=true
+
+# Execution
--- app/src/main/java/org/chickenhook/AndroidHackingTemplate/MainActivity.kt
+++ app/src/main/java/org/chickenhook/AndroidHackingTemplate/MainActivity.kt
@@ -2,13 +2,34 @@ package org.chickenhook.AndroidHackingTemplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.chickenhook.myandroidhackinglibrary.NativeInterface
+import android.view.View
+import android.widget.Button
+import org.chickenhook.AndroidHackingTemplate.penetration.ClipboardManagerCrash
+import org.chickenhook.AndroidHackingTemplate.penetration.PackageManagerCrash
+import org.chickenhook.AndroidHackingTemplate.penetration.ServiceManagerCrash
+
+//import org.chickenhook.myandroidhackinglibrary.NativeInterface
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        NativeInterface.installHooks()
+        //NativeInterface.installHooks()
+        findViewById<Button>(R.id.package_manager_attack)?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                PackageManagerCrash().launch(this@MainActivity)
+            }
+        })
+        findViewById<Button>(R.id.clipboard_manager_crash)?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                ClipboardManagerCrash().launch(this@MainActivity)
+            }
+        })
+        findViewById<Button>(R.id.service_manager_crash)?.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                ServiceManagerCrash().launch(this@MainActivity)
+            }
+        })
     }
 }
