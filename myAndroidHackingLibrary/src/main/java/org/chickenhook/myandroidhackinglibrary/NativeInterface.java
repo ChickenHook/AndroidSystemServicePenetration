8901a8647aac51f861451b0710d6270324515640
SarotecK <funker2013@gmail.com>
2022-10-25 20:49:55 +0200

    refactoring and package name add initial fastlane stuff

--- myAndroidHackingLibrary/src/main/java/org/chickenhook/myandroidhackinglibrary/NativeInterface.java
+++
@@ -1,9 +0,0 @@
-package org.chickenhook.myandroidhackinglibrary;
-
-public class NativeInterface {
-    static {
-        System.loadLibrary("myNativeLib");
-    }
-
-    public static native void installHooks();
-}
